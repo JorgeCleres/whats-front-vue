870a58656434a68fb21e94d4c252a1cc4cf5b6c5
JorgeCleres <jrgcleres@hotmail.com>
2022-01-20 22:41:05 -0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h5>Acesse o chat</h5>
+      <q-input rounded outlined v-model="email" lavel="informe seu email" bg-color="white" class="q-mb-md"></q-input>
+      <q-btn :riple="false" color="secondary" label="Acessar Chat"></q-btn>
+    </div>
+    <q-separator vertical></q-separator>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  data() {
+    return {
+      email: ""
+    }
+  }
+})
+</script>
